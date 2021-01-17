f95230da78976a7195792b6a6a2009b20cace574
Desta F Muchamad <desta.08b@gmail.com>
2021-01-18 04:18:58 +0700

    Update Axios.

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,22 +1,22 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
 // item
 import MovieItem from './MovieItem'
 
-const key = 'affcf0b4f1b2729cfa1a8fdb18e60c96';
-
-// req
-const getMovieData = async () => {
-    const res = await axios.get(`https://api.themoviedb.org/3/trending/all/day?api_key=${key}`)
-    console.log(res);
-}
-
 export default function MovieList() {
 
-    getMovieData();
-
     const [items, setItems] = useState([]);
+
+    useEffect(() => {
+        const key = 'affcf0b4f1b2729cfa1a8fdb18e60c96';
+        // req
+        const getMovieData = async () => {
+            const res = await axios.get(`https://api.themoviedb.org/3/trending/all/day?api_key=${key}`)
+            setItems(res.data.results);
+        }
+        getMovieData();
+    }, [])
 
     return (
         <div>
