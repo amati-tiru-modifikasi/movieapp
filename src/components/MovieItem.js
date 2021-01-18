fdbffd1b1632f573053f579e77e402b1f0969278
mdestafadilah <desta.08b@gmail.com>
2021-01-18 15:44:27 +0700

    Done

--- src/components/MovieItem.js
+++ src/components/MovieItem.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from 'react';
+import {
+  SimpleGrid,
+  Box,
+  Avatar,
+  Flex,
+  Stack,
+  Heading,
+  Badge,
+} from '@chakra-ui/react';
 
-export default function MovieItem() {
-    return (
-        <div>
-            Movie Item
-        </div>
-    )
+const baseUrl = 'https://image.tmdb.org/t/p/w500/';
+
+export default function MovieItem({ item }) {
+  return (
+    <SimpleGrid columns={2} spacing={10}>
+      <Box p={12} shadow="md" borderWidth="1px" rounded="md" width="2xs">
+        <Flex justifyContent="center" paddingY="2">
+          <Avatar
+            size="2xl"
+            name="Segun Adebayo"
+            src={`${baseUrl}${item.poster_path}`}
+          />
+        </Flex>
+        <Stack>
+          <Heading size="md" fontWeight="semibold">
+            {item.name}
+          </Heading>
+        </Stack>
+      </Box>
+    </SimpleGrid>
+  );
 }
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,26 +1,29 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 // item
-import MovieItem from './MovieItem'
+import MovieItem from './MovieItem';
 
 export default function MovieList() {
+  const [items, setItems] = useState([]);
 
-    const [items, setItems] = useState([]);
+  useEffect(() => {
+    const key = 'affcf0b4f1b2729cfa1a8fdb18e60c96';
+    // req
+    const getMovieData = async () => {
+      const res = await axios.get(
+        `https://api.themoviedb.org/3/trending/all/day?api_key=${key}`
+      );
+      setItems(res.data.results);
+    };
+    getMovieData();
+  }, []);
 
-    useEffect(() => {
-        const key = 'affcf0b4f1b2729cfa1a8fdb18e60c96';
-        // req
-        const getMovieData = async () => {
-            const res = await axios.get(`https://api.themoviedb.org/3/trending/all/day?api_key=${key}`)
-            setItems(res.data.results);
-        }
-        getMovieData();
-    }, [])
-
-    return (
-        <div>
-            Movie List
-        </div>
-    )
+  return (
+    <div>
+      {items.map(item => (
+        <MovieItem key={item.id} item={item} />
+      ))}
+    </div>
+  );
 }
